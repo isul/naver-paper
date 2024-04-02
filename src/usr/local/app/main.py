d2162e9afa6fdf68938eac45147ae917a90e495e
isul <isul@isulnara.com>
2024-04-02 16:10:38 +0900

    소스 사이트에 https://damoang.net 추가

--- src/usr/local/app/main.py
+++ src/usr/local/app/main.py
@@ -2,11 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 import argparse
 
-def find_naver_campaign_links(base_url, visited_urls_file='visited_urls.txt'):
+def find_naver_campaign_links(source_urls, visited_urls_file='visited_urls.txt'):
+    # Read Source URLs from file
+    if source_urls == []:
+        return
+
     # Read visited URLs from file
     try:
         with open(visited_urls_file, 'r') as file:
@@ -14,38 +19,55 @@ def find_naver_campaign_links(base_url, visited_urls_file='visited_urls.txt'):
     except FileNotFoundError:
         visited_urls = set()
 
-    # Send a request to the base URL
-    response = requests.get(base_url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Find all span elements with class 'list_subject' and get 'a' tags
-    list_subject_links = soup.find_all('span', class_='list_subject')
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
     naver_links = []
-    for span in list_subject_links:
-        a_tag = span.find('a', href=True)
-        if a_tag and '네이버' in a_tag.text:
-            naver_links.append(a_tag['href'])
+    for base_url in source_urls:
+      # Send a request to the base URL
+      response = requests.get(base_url, verify=False)
+      soup = BeautifulSoup(response.text, 'html.parser')
+
+      if base_url.startswith("https://www.clien.net"):
+        # Find all span elements with class 'list_subject' and get 'a' tags
+        list_subject_links = soup.find_all('span', class_='list_subject')
+        for span in list_subject_links:
+          a_tag = span.find('a', href=True)
+          if a_tag and '네이버' in a_tag.text:
+            if a_tag['href'].startswith(base_url):
+              full_link = a_tag['href']
+            else:
+              full_link = urljoin(base_url, a_tag['href'])
+            naver_links.append(full_link)
+      elif base_url.startswith("https://damoang.net"):
+        # Find all <a> elements 
+        list_subject_links = soup.find_all('a')
+        for a_tag in list_subject_links:
+          if a_tag and '네이버' in a_tag.text and a_tag['href'].startswith(base_url):
+            if a_tag['href'].startswith(base_url):
+              full_link = a_tag['href']
+            else:
+              full_link = urljoin(base_url, a_tag['href'])
+            naver_links.append(full_link)
+ 
+    print(naver_links)
 
     # Initialize a list to store campaign links
     campaign_links = []
 
     # Check each Naver link
     for link in naver_links:
-        full_link = urljoin(base_url, link)
-        print("naver_links - " + full_link)
-        if full_link in visited_urls:
-            continue  # Skip already visited links
+      if link in visited_urls:
+        continue  # Skip already visited links
 
-        res = requests.get(full_link)
-        inner_soup = BeautifulSoup(res.text, 'html.parser')
+      res = requests.get(link, verify=False)
+      inner_soup = BeautifulSoup(res.text, 'html.parser')
 
-        # Find all links that start with the campaign URL
-        for a_tag in inner_soup.find_all('a', href=True):
-            if a_tag['href'].startswith("https://campaign2-api.naver.com") or a_tag['href'].startswith("https://ofw.adison.co"):
-                campaign_links.append(a_tag['href'])
+      # Find all links that start with the campaign URL
+      for a_tag in inner_soup.find_all('a', href=True):
+        if a_tag['href'].startswith("https://campaign2-api.naver.com") or a_tag['href'].startswith("https://ofw.adison.co"):
+          campaign_links.append(a_tag['href'])
 
-        # Add the visited link to the set
-        visited_urls.add(full_link)
+      # Add the visited link to the set
+      visited_urls.add(link)
 
     # Save the updated visited URLs to the file
     with open(visited_urls_file, 'w') as file:
@@ -57,10 +79,10 @@ def find_naver_campaign_links(base_url, visited_urls_file='visited_urls.txt'):
 
 def run(id, passwd) -> None:
     # The base URL to start with
-    base_url = "https://www.clien.net/service/board/jirum"
-    #base_url = "https://www.clien.net/service/board/park"
-
-    campaign_links = find_naver_campaign_links(base_url, "/data/visited_urls.txt")
+    source_urls = []
+    source_urls.append("https://www.clien.net/service/board/jirum")
+    source_urls.append("https://damoang.net/economy")
+    campaign_links = find_naver_campaign_links(source_urls, "/data/visited_urls.txt")
     if(campaign_links == []):
         print("모든 링크를 방문했습니다.")
         return
